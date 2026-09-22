ed21f8f6fc5f6bf180fa53caf2217296af61a263
SHRIYA V <shriyav2006@gmail.com>
2026-09-22 20:36:07 +0530

    Insert initial employee data into database

---
+++ insert_data.sql
@@ -0,0 +1,25 @@
+USE employee_analytics;
+
+INSERT INTO employees
+(employee_id, employee_name, department, position, gender, age, experience, salary, joining_date, performance_score, promotion)
+VALUES
+('E001','Arun','IT','Developer','Male',24,2,65000,'2024-06-10',4.2,'Yes'),
+('E002','Priya','HR','HR Executive','Female',27,4,52000,'2022-08-15',4.5,'No'),
+('E003','Rahul','IT','Backend Developer','Male',29,5,78000,'2021-03-20',4.7,'Yes'),
+('E004','Sneha','Finance','Financial Analyst','Female',26,3,60000,'2023-01-12',4.1,'No'),
+('E005','Vikram','IT','Data Analyst','Male',31,7,85000,'2019-07-18',4.8,'Yes'),
+('E006','Ananya','Marketing','Marketing Executive','Female',25,2,48000,'2024-02-10',3.9,'No'),
+('E007','Karthik','Finance','Accountant','Male',30,6,70000,'2020-05-22',4.3,'Yes'),
+('E008','Divya','HR','Recruiter','Female',28,5,58000,'2021-11-05',4.4,'Yes'),
+('E009','Sanjay','IT','Software Engineer','Male',27,4,72000,'2022-04-14',4.0,'No'),
+('E010','Meena','Marketing','Marketing Manager','Female',34,10,92000,'2016-09-25',4.9,'Yes'),
+('E011','Rohit','Finance','Finance Manager','Male',36,12,98000,'2014-06-18',4.8,'Yes'),
+('E012','Keerthi','HR','HR Manager','Female',35,11,90000,'2015-03-12',4.7,'Yes'),
+('E013','Ajay','IT','Senior Developer','Male',33,9,95000,'2017-08-21',4.6,'Yes'),
+('E014','Harini','Marketing','Content Executive','Female',23,1,42000,'2025-01-10',3.8,'No'),
+('E015','Manoj','IT','Tester','Male',26,3,55000,'2023-09-01',3.7,'No'),
+('E016','Pooja','Finance','Financial Analyst','Female',29,5,64000,'2021-06-15',4.2,'No'),
+('E017','Surya','IT','DevOps Engineer','Male',32,8,88000,'2018-04-11',4.5,'Yes'),
+('E018','Nithya','HR','HR Executive','Female',24,2,50000,'2024-07-19',4.0,'No'),
+('E019','Aravind','Marketing','Sales Executive','Male',28,4,56000,'2022-12-03',4.1,'No'),
+('E020','Swetha','Finance','Accountant','Female',27,3,57000,'2023-05-17',4.0,'No');
